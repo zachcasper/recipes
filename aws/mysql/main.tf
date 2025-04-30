c2dc15256778e66678fc7c5d782fee3d30d6611c
Zach Casper <zachcasper@microsoft.com>
2025-04-30 06:16:56 -0700

    Redid from tutorial
    Signed-off-by: Zach Casper <zachcasper@microsoft.com>

--- aws/mysql/main.tf
+++ aws/mysql/main.tf
@@ -12,20 +12,43 @@ variable "context" {
   type = any
 }
 
+data "aws_availability_zones" "available" {}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.77.0"
 
-  name                 = var.context.application.name
+  name                 = "mysql"
   cidr                 = "10.27.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
-resource "aws_db_subnet_group" "education" {
-  name       = var.context.application.name
+resource "aws_db_subnet_group" "mysql" {
+  name       = "mysql"
   subnet_ids = module.vpc.public_subnets
 }
 
+resource "aws_security_group" "rds" {
+  name   = "mysql"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_db_instance" "mysql" {
   engine               = "mysql"
@@ -36,6 +59,8 @@ resource "aws_db_instance" "mysql" {
   username             = var.context.application.name
   password             = "WU9VUl9QQVNTV09SRA=="
   parameter_group_name = "default.mysql8.0"
+  db_subnet_group_name   = aws_db_subnet_group.education.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
   skip_final_snapshot  = true
   publicly_accessible =  true
 }
