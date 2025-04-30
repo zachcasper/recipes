53efbb5066827c70069b7a2dd2542bf291a086d7
Zach Casper <zachcasper@microsoft.com>
2025-04-30 09:03:47 -0700

    Fixed port number on security group
    Signed-off-by: Zach Casper <zachcasper@microsoft.com>

--- aws/mysql/main.tf
+++ aws/mysql/main.tf
@@ -36,15 +36,15 @@ resource "aws_security_group" "rds" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port   = 5432
-    to_port     = 5432
+    from_port   = 3306
+    to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 5432
-    to_port     = 5432
+    from_port   = 3306
+    to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
