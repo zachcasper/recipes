33fe9e34cf293b99a22a8f9ec218c51d65709fcf
Zach Casper <zachcasper@microsoft.com>
2025-04-30 06:10:02 -0700

    Removed classic params from vpc
    Signed-off-by: Zach Casper <zachcasper@microsoft.com>

--- aws/mysql/main.tf
+++ aws/mysql/main.tf
@@ -20,8 +20,6 @@ module "vpc" {
   cidr                 = "10.27.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_dns_hostnames = true
-  enable_dns_support   = true
 }
 
 resource "aws_db_subnet_group" "education" {
