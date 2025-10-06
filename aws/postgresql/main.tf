a3cb8a646acd6ee97a3708267f825adaa70240cd
Zach Casper <zcasper@gmail.com>
2025-10-06 17:06:31 -0500

    Update main.tf

--- aws/postgresql/main.tf
+++ aws/postgresql/main.tf
@@ -57,7 +57,7 @@ resource "aws_db_instance" "postgres" {
   allocated_storage      = 5
   engine                 = "postgres"
   engine_version         = "14"
-  username               = var.context.application.name
+  username               = "postgresql_user"
   password               = "WU9VUl9QQVNTV09SRA=="
   db_subnet_group_name   = aws_db_subnet_group.postgresql.name
   vpc_security_group_ids = [aws_security_group.rds.id]
