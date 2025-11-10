2d87618866f1b4dd2f6092b6acdd732478102879
Zach Casper <zcasper@gmail.com>
2025-11-10 06:51:32 -0600

    Refactor Redis cache configuration to enable public access
    Removed private endpoint and DNS zone resources for Redis cache.

--- azure/redis/main.tf
+++ azure/redis/main.tf
@@ -27,16 +27,6 @@ variable "location" {
   type = string
 }
 
-variable "vnet_id" {
-  description = "The vnet ID to create a private endpoint"
-  type = string
-}
-
-variable "subnet_id" {
-  description = "The subnet ID to create a private endpoint"
-  type = string
-}
-
 resource "azurerm_redis_cache" "redis" {
   name                          = module.naming.redis_cache.name_unique
   location                      = var.location
@@ -46,39 +36,7 @@ resource "azurerm_redis_cache" "redis" {
   sku_name                      = "Basic"
   enable_non_ssl_port           = false
   minimum_tls_version           = "1.2"
-  public_network_access_enabled = false
-}
-
-resource "azurerm_private_endpoint" "endpoint" {
-  name                = module.naming.private_endpoint.name_unique
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  subnet_id           = var.subnet_id
-
-  private_service_connection {
-    name                           = module.naming.private_service_connection.name_unique
-    private_connection_resource_id = azurerm_redis_cache.redis.id
-    is_manual_connection           = false
-    subresource_names              = ["redisCache"]
-  }
-
-  private_dns_zone_group {
-    name                 = module.naming.private_dns_zone_group.name_unique
-    private_dns_zone_ids = [azurerm_private_dns_zone.dns_zone.id]
-  }
-
-}
-
-resource "azurerm_private_dns_zone" "dns_zone" {
-  name                = "privatelink.redis.cache.windows.net"
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "dns_zone_link" {
-  name                  = "todolist-dns-zone-link"
-  resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.dns_zone.name
-  virtual_network_id    = var.vnet_id
+  public_network_access_enabled = true
 }
 
 output "result" {
