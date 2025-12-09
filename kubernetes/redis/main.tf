e85054780e95b982dc0a6221c03bdb6c3a696c2b
Zach Casper <zcasper@gmail.com>
2025-12-09 13:45:31 -0600

    Update Redis service and deployment outputs

--- kubernetes/redis/main.tf
+++ kubernetes/redis/main.tf
@@ -120,7 +120,7 @@ resource "kubernetes_service" "redis" {
 output "result" {
   value = {
     values = {
-      host = "${kubernetes_service.metadata.name}.${kubernetes_service.metadata.namespace}.svc.cluster.local"
+      host = "${kubernetes_service.redis.metadata.name}.${kubernetes_service.redis.metadata.namespace}.svc.cluster.local"
       port = kubernetes_service.spec.port[0].port
       username = ""
     }
@@ -129,8 +129,8 @@ output "result" {
     }
     // UCP resource IDs
     resources = [
-        "/planes/kubernetes/local/namespaces/${kubernetes_service.metadata.namespace}/providers/core/Service/${kubernetes_service.metadata.name}",
-        "/planes/kubernetes/local/namespaces/${kubernetes_deployment.metadata.namespace}/providers/apps/Deployment/${kubernetes_deployment.metadata.name}"
+        "/planes/kubernetes/local/namespaces/${kubernetes_service.redis.metadata.namespace}/providers/core/Service/${kubernetes_service.redis.metadata.name}",
+        "/planes/kubernetes/local/namespaces/${kubernetes_deployment.redis.metadata.namespace}/providers/apps/Deployment/${kubernetes_deployment.redis.metadata.name}"
     ]
   }
   description = "The result of the Recipe. Must match the target resource's schema."
